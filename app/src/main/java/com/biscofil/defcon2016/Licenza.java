3189d826642dd87783033e4732221a7bc0965567
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-01-16 20:30:14 +0100

    update

---
+++ app/src/main/java/com/biscofil/defcon2016/Licenza.java
@@ -0,0 +1,37 @@
+package com.biscofil.defcon2016;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by bisco on 16/01/2017.
+ */
+
+public class Licenza {
+
+    public int id;
+    public String licenza;
+    public String link_licenza;
+    public String descrizione;
+
+    public Licenza() {
+    }
+
+    public Licenza(int id, String licenza, String link_licenza, String descrizione) {
+        this.id = id;
+        this.licenza = licenza;
+        this.link_licenza = link_licenza;
+        this.descrizione = descrizione;
+    }
+
+    public void parse(JSONObject obj) {
+        try {
+            this.id = obj.getInt("id");
+            this.licenza = obj.getString("licenza");
+            this.link_licenza = obj.getString("link_licenza");
+            this.descrizione = obj.getString("descrizione");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/biscofil/defcon2016/fragments/Licenze_fragment.java
+++ app/src/main/java/com/biscofil/defcon2016/fragments/Licenze_fragment.java
@@ -1,12 +1,28 @@
 package com.biscofil.defcon2016.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.biscofil.defcon2016.Licenza;
+import com.biscofil.defcon2016.LicenzeAdapter;
 import com.biscofil.defcon2016.R;
+import com.biscofil.defcon2016.XhrInterface;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Licenze_fragment extends Fragment {
 
@@ -17,7 +33,70 @@ public class Licenze_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_licenze, container, false);
         getActivity().setTitle(getString(R.string.licenze_fragment_title));
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list_licenze);
+
+        new MyTask(this, getActivity(), listView).execute();
+
         return rootView;
+    }
+
+
+    private class MyTask extends AsyncTask<Void, Void, List> {
+
+        ListView listView;
+        Fragment fra;
+        Activity act;
+
+        public MyTask(Fragment f, Activity a, ListView lv) {
+            fra = f;
+            act = a;
+            listView = lv;
+        }
+
+        @Override
+        protected void onPostExecute(final List l) {
+            super.onPostExecute(l);
+            LicenzeAdapter adapter = new LicenzeAdapter(act, R.layout.licenze_row, l);
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Licenza o = (Licenza) l.get(position);
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(o.link_licenza));
+                    startActivity(i);
+                }
+            });
+        }
+
+        @Override
+        protected List doInBackground(Void... params) {
+            try {
+                try {
+                    String url = getString(R.string.web_url) + getString(R.string.xhr_controller) + getString(R.string.xhr_licenze);
+                    JSONArray data = new XhrInterface().getArray(url);
+
+                    List list = new LinkedList();
+
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject object = data.getJSONObject(i);
+
+                        Licenza l = new Licenza();
+                        l.parse(object);
+                        list.add(l);
+                    }
+
+                    return list;
+                } catch (Exception e) {
+                    Log.e("ECOME", e.getLocalizedMessage());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
 }
