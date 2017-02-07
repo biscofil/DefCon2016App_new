bb34436d63fdf5f735d50309b491e9f68cf99959
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-02-07 22:24:53 +0100

    update

--- app/src/main/java/com/biscofil/defcon2016/Details_activity.java
+++ app/src/main/java/com/biscofil/defcon2016/Details_activity.java
@@ -10,6 +10,10 @@ import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.Random;
 
 public class Details_activity extends AppCompatActivity {
@@ -39,5 +43,35 @@ public class Details_activity extends AppCompatActivity {
         rb.setMax(5);
         Random r = new Random();
         rb.setRating(r.nextFloat() * 5);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        // first series is a line
+        DataPoint[] points = new DataPoint[100];
+        for (int k = 0; k < points.length; k++) {
+            points[k] = new DataPoint(k, Math.sin(k * 0.5) * 20 * (Math.random() * 10 + 1));
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
+
+        // set manual X bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-150);
+        graph.getViewport().setMaxY(150);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(4);
+        graph.getViewport().setMaxX(80);
+
+        // enable scaling and scrolling
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        graph.addSeries(series);
+
+        graph.getViewport().setScrollable(true); // enables horizontal scrolling
+        graph.getViewport().setScrollableY(true); // enables vertical scrolling
+        graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+        graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
+
     }
 }
