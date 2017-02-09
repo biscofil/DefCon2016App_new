948bf6cfcb5fccf8630afcd3ddaf2878e6025378
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-02-09 15:44:36 +0100

    update

--- app/src/main/java/com/biscofil/defcon2016/Details_activity.java
+++ app/src/main/java/com/biscofil/defcon2016/Details_activity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
@@ -44,6 +46,12 @@ public class Details_activity extends AppCompatActivity {
 
         setContentView(R.layout.activity_details);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         Intent i = getIntent();
         final Struttura s = (Struttura) i.getSerializableExtra("struttura");
 
@@ -69,6 +77,18 @@ public class Details_activity extends AppCompatActivity {
 
         //storico
         setup_graph(s);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void setup_graph(Struttura s) {
