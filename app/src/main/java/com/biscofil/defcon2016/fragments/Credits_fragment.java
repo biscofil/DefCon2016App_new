9dc1ab0f23ff5eb19ab84665f45155e9fb035400
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-02-15 22:08:17 +0100

    aggiornati credits con link a linkedin aggiunta listview per licenze librerie

--- app/src/main/java/com/biscofil/defcon2016/fragments/Credits_fragment.java
+++ app/src/main/java/com/biscofil/defcon2016/fragments/Credits_fragment.java
@@ -1,10 +1,13 @@
 package com.biscofil.defcon2016.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.biscofil.defcon2016.R;
 
@@ -12,11 +15,43 @@ public class Credits_fragment extends Fragment {
 
     public Credits_fragment() {
     }
-/* commento di prova*/
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_credits, container, false);
         getActivity().setTitle(getString(R.string.credits_fragment_title));
+
+        Button btn_cabianca = (Button) rootView.findViewById(R.id.btn_cabianca);
+        btn_cabianca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(getString(R.string.linkedin_cabianca)));
+                startActivity(i);
+            }
+        });
+
+        Button btn_bisconcin = (Button) rootView.findViewById(R.id.btn_bisconcin);
+        btn_bisconcin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(getString(R.string.linkedin_bisconcin)));
+                startActivity(i);
+            }
+        });
+
+
+        Button btn_busolin = (Button) rootView.findViewById(R.id.btn_busolin);
+        btn_busolin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(getString(R.string.linkedin_busolin)));
+                startActivity(i);
+            }
+        });
+
 
         return rootView;
     }
