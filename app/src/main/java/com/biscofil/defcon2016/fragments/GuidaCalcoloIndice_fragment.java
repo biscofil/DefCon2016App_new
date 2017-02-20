78dbfea1b2e4f167ccb73fd70367f5fb141bc4e9
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-02-20 12:03:12 +0100

    update

--- app/src/main/java/com/biscofil/defcon2016/fragments/GuidaCalcoloIndice_fragment.java
+++ app/src/main/java/com/biscofil/defcon2016/fragments/GuidaCalcoloIndice_fragment.java
@@ -19,7 +19,7 @@ public class GuidaCalcoloIndice_fragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_guida_calcolo_indice, container, false);
         getActivity().setTitle(getString(R.string.calcolo_fragment_title));
         WebView calcolo_webview = (WebView) rootView.findViewById(R.id.calcolo_webview);
-        calcolo_webview.loadUrl(getString(R.string.webview_calcolo_url));
+        calcolo_webview.loadUrl(getString(R.string.web_url) + getString(R.string.webview_calcolo_url));
         return rootView;
     }
 }
